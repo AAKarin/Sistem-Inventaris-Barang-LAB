2f20a9b556c1e4beee4ae9f97c39b396fdcc1ac4
MYN2997 <hawkinsdamien007@gmail.com>
2026-05-07 06:22:53 +0700

    menambahkan CRUD di api barang

--- app/Controllers/Api/Barang.php
+++ app/Controllers/Api/Barang.php
@@ -10,7 +10,6 @@ class Barang extends ResourceController
     protected $modelName = 'App\Models\BarangModel';
     protected $format    = 'json';
 
-    // Menampilkan semua data barang (GET)
     public function index()
     {
         $data = $this->model->getBarangDenganKategori();
@@ -22,7 +21,6 @@ class Barang extends ResourceController
         ]);
     }
 
-    // Menampilkan detail satu barang berdasarkan ID (GET)
     public function show($id = null)
     {
         $data = $this->model->find($id);
@@ -30,5 +28,55 @@ class Barang extends ResourceController
             return $this->respond($data);
         }
         return $this->failNotFound('Barang tidak ditemukan');
+    }
+
+    public function create()
+    {
+        $data = $this->request->getJSON(true) ?? $this->request->getPost();
+
+        if ($this->model->insert($data)) {
+            return $this->respondCreated([
+                'status' => 201,
+                'message' => 'Data barang berhasil ditambahkan'
+            ]);
+        }
+
+        return $this->fail('Gagal menambahkan data barang');
+    }
+
+    public function update($id = null)
+    {
+        $cekBarang = $this->model->find($id);
+        if (!$cekBarang) {
+            return $this->failNotFound('Barang tidak ditemukan untuk diupdate');
+        }
+
+        $data = $this->request->getJSON(true) ?? $this->request->getRawInput();
+
+        if ($this->model->update($id, $data)) {
+            return $this->respond([
+                'status' => 200,
+                'message' => 'Data barang berhasil diupdate'
+            ]);
+        }
+
+        return $this->fail('Gagal mengupdate data barang');
+    }
+
+    public function delete($id = null)
+    {
+        $cekBarang = $this->model->find($id);
+        if (!$cekBarang) {
+            return $this->failNotFound('Barang tidak ditemukan untuk dihapus');
+        }
+
+        if ($this->model->delete($id)) {
+            return $this->respondDeleted([
+                'status' => 200,
+                'message' => 'Data barang berhasil dihapus'
+            ]);
+        }
+
+        return $this->fail('Gagal menghapus data barang');
     }
 }
